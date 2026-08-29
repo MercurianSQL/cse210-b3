9b12792f383bd2e740dab4d4d4891a0c20aee790
Marinda Keller <mkeller@byupatheway.edu>
2026-08-29 13:41:22 -0600

    old

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 3 Project.");
+        int magicNumber = 0;
+        int guess = -1;
+        Console.WriteLine("Try to guess the Magic Number!");
+        Console.Write("What is the Magic Number? ");
+        magicNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine($"TESTING: Your Magic Number is {magicNumber}!");
+        string response;
+
+        do
+        {
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "no");
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("Congratulations! You guessed the Magic Number!");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher...");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower...");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong with your guess. Please try again.");
+            }
     }
 }
